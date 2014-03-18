4d5d87e864364269abe62a27e076c10bd4377c8e
clutcher <iclutcher@gmail.com>
2014-03-18 22:33:02 +0200

    Fix network build error.

--- main.py
+++ main.py
@@ -586,11 +586,76 @@ class GameMap(object):
 
         return 1
 
+    def add_edges_to_diagonal_and_through_for_recognition(self):
+        for node in self.diagonalNodes:
+            i = node[0]
+            j = node[1]
+
+            edge = ((i, j), (i + 1, j))
+            edge2 = ((i, j), (i, j - 1))
+            edge3 = ((i, j), (i, j + 1))
+            edge4 = ((i, j), (i - 1, j))
+
+            self.G.add_edge(*edge)
+            self.G.add_edge(*edge2)
+            self.G.add_edge(*edge3)
+            self.G.add_edge(*edge4)
+
+        for node in self.throughNodes:
+            i = node[0]
+            j = node[1]
+
+            edge = ((i, j), (i + 1, j))
+            edge2 = ((i, j), (i, j - 1))
+            edge3 = ((i, j), (i, j + 1))
+            edge4 = ((i, j), (i - 1, j))
+
+            self.G.add_edge(*edge)
+            self.G.add_edge(*edge2)
+            self.G.add_edge(*edge3)
+            self.G.add_edge(*edge4)
+
+        return 0
+
+    def clean_added_edges(self):
+        for node in self.diagonalNodes:
+            edges = self.G.edges(node)
+
+            for edge in edges:
+                self.G.remove_edge(*edge)
+
+        for node in self.throughNodes:
+            i = node[0]
+            j = node[1]
+
+            edge = ((i, j), (i + 1, j))
+            edge2 = ((i, j), (i - 1, j))
+            edge3 = ((i, j + 1), (i, j - 1))
+
+            edge4 = ((i, j), (i, j - 1))
+            edge5 = ((i, j), (i, j + 1))
+            edge6 = ((i - 1, j), (i + 1, j))
+
+            firstVariantCheck = self.check_edge_in_edgelist(
+                edge) and self.check_edge_in_edgelist(edge2) and self.check_edge_in_edgelist(edge3)
+            secondVariantCheck = self.check_edge_in_edgelist(
+                edge4) and self.check_edge_in_edgelist(edge5) and self.check_edge_in_edgelist(edge6)
+
+            if firstVariantCheck:
+                self.G.remove_edge(*edge4)
+                self.G.remove_edge(*edge5)
+            elif secondVariantCheck:
+                self.G.remove_edge(*edge)
+                self.G.remove_edge(*edge2)
+
+        return 0
 
     def get_final_tiles(self, nodeCheck=0, output=True):
         tiles = []
 
+        self.add_edges_to_diagonal_and_through_for_recognition()
         if output:
+
             for i in range(1, self.n - 1):
                 for j in range(1, self.m - 1):
                     node = (i, j)
@@ -598,23 +663,24 @@ class GameMap(object):
                         print str(node) + ' - tile 5'
                     elif node in self.diagonalNodes:
                         print str(node) + ' - tile 3'
-                    elif self.G.degree(node) == 2:
+                    elif self.get_simple_degree(node) == 2:
                         print str(node) + ' - tile 1'
-                    elif self.G.degree(node) == 3:
+                    elif self.get_simple_degree(node) == 3:
                         print str(node) + ' - tile 2'
-                    elif self.G.degree(node) == 4:
+                    elif self.get_simple_degree(node) == 4:
                         print str(node) + ' - tile 4'
+
         elif nodeCheck:
             node = nodeCheck
             if node in self.throughNodes:
                 tiles.append(5)
             elif node in self.diagonalNodes:
                 tiles.append(3)
-            elif self.G.degree(node) == 2:
+            elif self.get_simple_degree(node) == 2:
                 tiles.append(1)
-            elif self.G.degree(node) == 3:
+            elif self.get_simple_degree(node) == 3:
                 tiles.append(2)
-            elif self.G.degree(node) == 4:
+            elif self.get_simple_degree(node) == 4:
                 tiles.append(4)
             else:
                 tiles.append(-1)
@@ -626,13 +692,13 @@ class GameMap(object):
                         tiles.append(5)
                     elif node in self.diagonalNodes:
                         tiles.append(3)
-                    elif self.G.degree(node) == 2:
+                    elif self.get_simple_degree(node) == 2:
                         tiles.append(1)
-                    elif self.G.degree(node) == 3:
+                    elif self.get_simple_degree(node) == 3:
                         tiles.append(2)
-                    elif self.G.degree(node) == 4:
+                    elif self.get_simple_degree(node) == 4:
                         tiles.append(4)
-
+        self.clean_added_edges()
         return tiles
 
     def check_excluded(self):
@@ -707,69 +773,6 @@ class GameMap(object):
     def output_image(self):
         import Image
 
-        def add_edges_to_diagonal_and_through_for_recognition():
-            for node in self.diagonalNodes:
-                i = node[0]
-                j = node[1]
-
-                edge = ((i, j), (i + 1, j))
-                edge2 = ((i, j), (i, j - 1))
-                edge3 = ((i, j), (i, j + 1))
-                edge4 = ((i, j), (i - 1, j))
-
-                self.G.add_edge(*edge)
-                self.G.add_edge(*edge2)
-                self.G.add_edge(*edge3)
-                self.G.add_edge(*edge4)
-
-            for node in self.throughNodes:
-                i = node[0]
-                j = node[1]
-
-                edge = ((i, j), (i + 1, j))
-                edge2 = ((i, j), (i, j - 1))
-                edge3 = ((i, j), (i, j + 1))
-                edge4 = ((i, j), (i - 1, j))
-
-                self.G.add_edge(*edge)
-                self.G.add_edge(*edge2)
-                self.G.add_edge(*edge3)
-                self.G.add_edge(*edge4)
-
-            return 0
-
-        def clean_added_edges():
-            for node in self.diagonalNodes:
-                edges = self.G.edges(node)
-
-                for edge in edges:
-                    self.G.remove_edge(*edge)
-
-            for node in self.throughNodes:
-                i = node[0]
-                j = node[1]
-
-                edge = ((i, j), (i + 1, j))
-                edge2 = ((i, j), (i - 1, j))
-                edge3 = ((i, j + 1), (i, j - 1))
-
-                edge4 = ((i, j), (i, j - 1))
-                edge5 = ((i, j), (i, j + 1))
-                edge6 = ((i - 1, j), (i + 1, j))
-
-                firstVariantCheck = self.check_edge_in_edgelist(
-                    edge) and self.check_edge_in_edgelist(edge2) and self.check_edge_in_edgelist(edge3)
-                secondVariantCheck = self.check_edge_in_edgelist(
-                    edge4) and self.check_edge_in_edgelist(edge5) and self.check_edge_in_edgelist(edge6)
-
-                if firstVariantCheck:
-                    self.G.remove_edge(*edge4)
-                    self.G.remove_edge(*edge5)
-                elif secondVariantCheck:
-                    self.G.remove_edge(*edge)
-                    self.G.remove_edge(*edge2)
-
-            return 0
 
         def get_rotation(node):
             i = node[0]
@@ -784,7 +787,7 @@ class GameMap(object):
 
             angle = 0
             if tile[0] == 1:
-                add_edges_to_diagonal_and_through_for_recognition()
+                self.add_edges_to_diagonal_and_through_for_recognition()
                 if self.check_edge_in_edgelist(edge) and self.check_edge_in_edgelist(edge2):
                     angle = 0
                 elif self.check_edge_in_edgelist(edge) and self.check_edge_in_edgelist(edge3):
@@ -794,11 +797,11 @@ class GameMap(object):
                 elif self.check_edge_in_edgelist(edge2) and self.check_edge_in_edgelist(edge4):
                     angle = 270
 
-                clean_added_edges()
+                self.clean_added_edges()
                 return angle
 
             elif tile[0] == 2:
-                add_edges_to_diagonal_and_through_for_recognition()
+                self.add_edges_to_diagonal_and_through_for_recognition()
                 if self.check_edge_in_edgelist(edge) and self.check_edge_in_edgelist(
                         edge2) and self.check_edge_in_edgelist(
                         edge3):
@@ -813,7 +816,7 @@ class GameMap(object):
                         edge3) and self.check_edge_in_edgelist(edge4):
                     angle = 270
 
-                clean_added_edges()
+                self.clean_added_edges()
                 return angle
 
             elif tile[0] == 3:
@@ -931,11 +934,11 @@ exclude = []
 #     for j in xrange(6, 11):
 #         node = (i, j)
 #         exclude.append(node)
-
+#
 # triangleLeft = [(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (3, 1)]
 # triangleRight = [(8, 10), (8, 9), (8, 8), (7, 10), (7, 9), (6, 10)]
 # exclude.extend(triangleLeft)
 # exclude.extend(triangleRight)
 
-map = GameMap(3, 3, difficultCoefficient=0.2, exclude=exclude, allTileTypes=True)
+map = GameMap(5, 5, difficultCoefficient=0.2, exclude=exclude, allTileTypes=True)
 map.make_map()
